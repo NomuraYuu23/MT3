199091370fefb45923afe26c1e46a2febee2e013
野村優羽 <@MSI>
2023-12-08 15:19:21 +0900

    点から線分までの最短距離問題

--- ShortestDistance.cpp
+++ ShortestDistance.cpp
@@ -9,7 +9,7 @@ ShortestDistance* ShortestDistance::GetInstance()
 float ShortestDistance::PointLineDist(const Vector3& p, const Line& l, Vector3& h, float& t)
 {
 
-	float lenSqV = Length(l.diff);
+	float lenSqV = Length(Normalize(l.diff));
 	t = 0.0f;
 	if (lenSqV > 0.0f) {
 		t = Dot(l.diff, Subtract(p, l.origin)) / lenSqV;
@@ -20,3 +20,27 @@ float ShortestDistance::PointLineDist(const Vector3& p, const Line& l, Vector3& 
 	return Length( Subtract(h, p));
 
 }
+
+float ShortestDistance::PointSegmentDist(const Vector3& p, const Segment& seg, Vector3& h, float& t)
+{
+
+	const Vector3 e = Add(seg.origin, seg.diff);
+
+	// 垂線の長さ、垂線の足の座標およびtを算出
+	float len = PointLineDist(p, Line(seg.origin, Subtract(e, seg.origin)), h, t);
+
+	if (Dot(Subtract(p, seg.origin), Subtract(e, seg.origin)) < 0.0f) {
+		// 始点側の外側
+		h = seg.origin;
+		return Length(Subtract(seg.origin, p));
+	}
+	else if (Dot(Subtract(p, e), Subtract(seg.origin, e)) < 0.0f) {
+		// 終点側の外側
+		h = e;
+		return Length(Subtract(e, p));
+	}
+
+	return len;
+
+}
+
--- ShortestDistance.h
+++ ShortestDistance.h
@@ -23,6 +23,16 @@ public:
     /// <returns>最短距離</returns>
     static float PointLineDist(const Vector3& p, const Line& l, Vector3& h, float &t);
 
+    /// <summary>
+    /// 点と線分の距離
+    /// </summary>
+    /// <param name="p">点</param>
+    /// <param name="l">線分</param>
+    /// <param name="h">最短距離となる端点(戻り値)</param>
+    /// <param name="t">端点位置(戻り値)</param>
+    /// <returns>最短距離</returns>
+    static float PointSegmentDist(const Vector3& p, const Segment& seg, Vector3& h, float& t);
+
 private:
     ShortestDistance() = default;
     ~ShortestDistance() = default;
